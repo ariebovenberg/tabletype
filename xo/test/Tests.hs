6ac3f34221f501cda00d99fd8ff50641cc323354
Arie Bovenberg <ariebovenberg@wlnss.com>
2018-06-20 21:48:00 +0200

    add another test

--- xo/test/Tests.hs
+++ xo/test/Tests.hs
@@ -1,16 +1,26 @@
 import qualified Data.Sequence
-import           Test.Tasty       (defaultMain, testGroup)
+import           Test.Tasty
 import           Test.Tasty.HUnit (assertEqual, testCase)
 import           Xo               (isWinner)
 
 main :: IO ()
 main = defaultMain unitTests
 
+unitTests :: TestTree
 unitTests =
   testGroup
     "unit tests"
-    [nobodyWinsEmptyBoard]
+    [nobodyWinsEmptyBoard, winningDiagonally]
 
 
+nobodyWinsEmptyBoard :: TestTree
 nobodyWinsEmptyBoard =
-  testCase "X is diagonal winner" $ assertEqual [] False $ isWinner (Data.Sequence.replicate 9 False)
+  testCase "nobody wins an empty board" $ assertEqual [] False
+    $ isWinner (Data.Sequence.replicate 9 False)
+
+winningDiagonally :: TestTree
+winningDiagonally =
+  testCase "diagonal win" $ assertEqual [] True
+    $ isWinner (Data.Sequence.fromList [False, False, True,
+                                        False, True, False,
+                                        True, False, False])
